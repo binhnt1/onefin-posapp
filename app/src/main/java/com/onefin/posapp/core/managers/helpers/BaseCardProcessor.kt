19ebb03c19b9851647b29bc4562843febc836c74
binhnt <binhnt1@gmail.com>
2025-12-03 21:58:05 +0700

    fixed

--- app/src/main/java/com/onefin/posapp/core/managers/helpers/BaseCardProcessor.kt
+++ app/src/main/java/com/onefin/posapp/core/managers/helpers/BaseCardProcessor.kt
@@ -170,10 +170,7 @@ abstract class BaseCardProcessor(
     }
 
     protected fun createBundle(): Bundle {
-        var flowType: Int = AidlConstants.EMV.FlowType.TYPE_EMV_STANDARD
-        if (cardType == AidlConstants.CardType.NFC)
-            flowType = AidlConstants.EMV.FlowType.TYPE_EMV_BRIEF
-
+        val flowType: Int = AidlConstants.EMV.FlowType.TYPE_EMV_STANDARD
         return Bundle().apply {
             putInt("flowType", flowType)
             putString("transType", "00")
